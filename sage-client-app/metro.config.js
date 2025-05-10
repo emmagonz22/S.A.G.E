10a3c86423096323e33d42a1272abdac9220405c
Emmanuel Gonzalez Morales <emmanuel.gonzalez22@upr.edu>
2025-05-10 16:52:24 -0400

    Mobile application moved from the S.A.G.E-ESP to S.A.G.E-ClientApp

--- sage-client-app/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withTamagui } = require("@tamagui/metro-plugin");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = withTamagui(config, {
-  components: ["tamagui"],
-  config: "./tamagui.config.ts",
-});
